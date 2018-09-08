beaaa0aaf03d9fd2d2d65bf7fc2268c0f0a00774
matheusrcruz <git.dev@outlook.com>
2018-09-07 22:27:20 -0300

    ADD:Middleware

--- node-mongo-compose/backend/app.js
+++ node-mongo-compose/backend/app.js
@@ -2,13 +2,18 @@ const express = require('express')
 const restful = require('node-restful')
 const server = express()
 const mongoose = restful.mongoose
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
 
 //Conect Database
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://db/mydb')
 
-// Teste
-server.get('/', (req, res, next) => res.send('Backend'))
+// Middlewares 
+server.use(bodyParser.urlencoded({extended:true}))
+
+
 
 // Start Server
 server.listen(3000)
